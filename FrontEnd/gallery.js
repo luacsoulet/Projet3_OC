c27d5d1f1636606b732a57ddaca6ba12ca4fd1c7
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2023-09-29 10:31:42 +0200

    Update style.css
    modification du code de récupération et de génération des projets via l'API.

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -3,7 +3,7 @@ let projets = window.localStorage.getItem('projets');
 
 if (projets == null) {
     // Récupération des projets depuis l'API
-    const reponse = await fetch("http://localhost:5678/api/works");
+    const reponse = await fetch("http://localhost:5678/api/works/");
     projets = await reponse.json();
     // Transformation des projets en JSON
     const valeurProjets = JSON.stringify(projets);
@@ -13,26 +13,21 @@ if (projets == null) {
     projets = JSON.parse(projets);
 }
 
+// Récupération de l'élément du DOM qui accueillera les projets
+const sectionGallery = document.querySelector(".gallery");
+
 function genererProjets(projets) {
     for (let i = 0; i < projets.length; i++) {
 
         const card = projets[i];
-        // Récupération de l'élément du DOM qui accueillera les projets
-        const sectionGallery = document.querySelector(".gallery");
-        // Création d’une balise dédiée à un projet 
-        const projetElement = document.createElement("figure");
-        projetElement.dataset.id = projets[i].id
-        // Création des balises
-        const imageElement = document.createElement("img");
-        imageElement.src = card.imageUrl;
-        imageElement.alt = card.title;
-        const figcaptionElement = document.createElement("figcaption");
-        figcaptionElement.innerText = card.title;
-        // Rattachement à la balise article a la section gallery
-        sectionGallery.appendChild(projetElement);
-        projetElement.appendChild(imageElement);
-        projetElement.appendChild(figcaptionElement);
+        sectionGallery.innerHTML += `
+            <figure id="${card.id}">
+                <img src=${card.imageUrl} alt=${card.title}>
+                <figcaption>${card.title}</figcaption>
+            </figure>`
     }
 }
 
+
 genererProjets(projets);
+
