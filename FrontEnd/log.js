43fe48b52c2dd2fc5b78f664afbaa65f004518b5
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2023-10-01 23:27:46 +0200

    Update log.js
    création de la fonction pour la requête de connexion a l'api et récupération des donner de la réponse.

---
+++ FrontEnd/log.js
@@ -0,0 +1,23 @@
+export function connexionFormulaire() {
+    const formulaireConnexion = document.querySelector(".formulaire-avis");
+    formulaireConnexion.addEventListener("submit", async function (event) {
+        event.preventDefault();
+        // Création de l’objet du nouvel avis.
+        const user = {
+            email: event.target.querySelector("[name=user_email]").value,
+            password: event.target.querySelector("[name=user_password]").value
+        };
+
+        const userLogIn = JSON.stringify(user);
+
+        const response = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: userLogIn
+        });
+
+        const token = await response.json();
+        const valeurToken = JSON.stringify(token);
+        window.sessionStorage.setItem("token", valeurToken);
+    });
+}
