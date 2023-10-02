ab66b5bdc59cc893756c2c58ebe13fe25053f03b
Lucas Magalhaes <lucasmagalhaes45200@gmail.com>
2023-10-02 02:29:22 +0200

    Update log.js
    implémentation de if else pour indiquer si les valeurs misent dans les champs sont bonnes ou fausses

--- FrontEnd/log.js
+++ FrontEnd/log.js
@@ -7,18 +7,22 @@ export function connexionFormulaire() {
             email: event.target.querySelector("[name=user_email]").value,
             password: event.target.querySelector("[name=user_password]").value
         };
+        if (user.email != "sophie.bluel@test.tld" || user.password != "S0phie") {
+            window.alert("E-mail et/ou mot de passe incorrect");
+        } else {
+            const userLogIn = JSON.stringify(user);
+            const response = await fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: userLogIn
+            });
 
-        const userLogIn = JSON.stringify(user);
+            const token = await response.json();
+            const valeurToken = JSON.stringify(token);
+            window.sessionStorage.setItem("token", valeurToken);
 
-        const response = await fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: userLogIn
-        });
-
-        const token = await response.json();
-        const valeurToken = JSON.stringify(token);
-        window.sessionStorage.setItem("token", valeurToken);
+            document.location.href = "./index.html";
+        }
     })
 }
 
