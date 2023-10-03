22c175a9f72c539a3c0b3f50b6448de0f4d3861b
Lucas Magalhaes <lucasmagalhaes45200@gmail.com>
2023-10-03 19:03:59 +0200

    index.html et editor.js
    Ajout du bandeau via javascript au-dessus du header et ajout de la balise font awesome dans le head de index.html pour avoir les icônes.

--- FrontEnd/editor.js
+++ FrontEnd/editor.js
@@ -1,3 +1,15 @@
 export async function modeEditor() {
     console.log("modif activer");
+    const headerDiv = document.querySelector("header");
+    const parentDiv = headerDiv.parentNode;
+    const bandeauEditor = document.createElement("div");
+    bandeauEditor.setAttribute("class", "bandeau");
+    const logoEditor = document.createElement("i");
+    logoEditor.setAttribute("class", "fa-regular fa-pen-to-square");
+    const textEditor = document.createElement("p");
+    textEditor.innerText = "Mode édition";
+
+    parentDiv.insertBefore(bandeauEditor, headerDiv);
+    bandeauEditor.appendChild(logoEditor);
+    bandeauEditor.appendChild(textEditor);
 };
