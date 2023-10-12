ba3b4a403b86cdb7b0a64d23e77f118d4590cda4
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2023-10-12 11:06:37 +0200

    Update app.js
    Rajout des commentaires

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -1,10 +1,13 @@
 import { genererGalerie } from './gallery.js';
 import { modeEditor } from './editor.js';
 
+//Géneration de la galerie
 genererGalerie();
 
+//Récupération du token dans le localStorage
 let token = window.localStorage.getItem('token');
 
+//Si le token est existant alors la fonction modeEditor est appeler
 if (token != null) {
     modeEditor();
 }
