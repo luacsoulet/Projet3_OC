b73e9ced77fa99cc50071e4194891c123cd22e45
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2023-10-12 11:06:02 +0200

    Update editor.js
    Ajout de la version final d'ajout de project au site via le formulaire. Et rajout des commentaires

--- FrontEnd/editor.js
+++ FrontEnd/editor.js
@@ -1,21 +1,24 @@
 
-export async function modeEditor() {
-
+export function modeEditor() {
+    //Sélection du header
     const headerDiv = document.querySelector("header");
     const parentDiv = headerDiv.parentNode;
 
+    //Création du bandeau apparaîssant lors de lu mode de modification activé
     const bandeauEditor = document.createElement("div");
     bandeauEditor.setAttribute("class", "bandeau");
     bandeauEditor.innerHTML = `
         <i class="fa-regular fa-pen-to-square"></i>
-        <p>Mode édition</p>
-    `
+        <p>Mode édition</p>`
 
+    // Insertion du bandeau avant le header de la page
     parentDiv.insertBefore(bandeauEditor, headerDiv);
 
+    //Création de div-mes-projets pout y mettre le titre Mes projets et la div contenant le bouton pour ouvrir la fenêtre de dialog
     const divMesprojets = document.createElement("div");
     divMesprojets.setAttribute("class", "div-mes-projets");
 
+    //Création de la div conetenant le bouton d'ouverture de la fenêtre de dialog
     const divModifications = document.createElement("div");
     divModifications.setAttribute("class", "div-modif");
     divModifications.setAttribute("class", "open-button");
@@ -23,24 +26,35 @@ export async function modeEditor() {
         <i class="fa-regular fa-pen-to-square"></i>
         <button class="button" id="open-button">modifier</button>
     `
-
+    // Sélection de la div gallery
     const gallery = document.querySelector(".gallery");
     const galleryParent = gallery.parentNode;
 
+    //Sélection de la div portfolio et du titre Mes projets
     const portfolio = document.querySelector("#portfolio");
     const titreH2 = document.querySelector("#portfolio h2");
 
+    //div-mes-projets est inserer dans #portfolio
     portfolio.appendChild(divMesprojets)
+
+    //ajout de la div-mes-projets avant le titreH2
     galleryParent.insertBefore(divMesprojets, titreH2);
+
+    //titreH2 devient enfant de divMesProjets
     divMesprojets.appendChild(titreH2);
+
+    //divModifications devient enfant de divMesProjets
     divMesprojets.appendChild(divModifications);
 
+    //Disparition des boutons de filtres par catégories
     const divButtons = document.querySelector(".buttons-filter").style.display = "none";
 
+    //Création de la balise dialog ayant pour class et id modal
     const dialogModal = document.createElement("dialog");
     dialogModal.setAttribute("class", "modal");
     dialogModal.setAttribute("id", "modal");
 
+    //Création de la modal Gallery affichant tout les projets et le boutton pour ajouter des projets
     const galleryModal = document.createElement("div");
     galleryModal.setAttribute("class", "modal-wrapper");
     galleryModal.classList.add("gallery-modal");
@@ -50,47 +64,64 @@ export async function modeEditor() {
             <div id="modal-gallery"></div>
             <button id="ajoutphoto">Ajouter une photo</button>`
 
+    //insertion de la dialog avant le header
     parentDiv.insertBefore(dialogModal, headerDiv);
+
+    //modal gallery devient enfant de la balise dialog
     dialogModal.appendChild(galleryModal);
 
-    // const addProjectModal = document.querySelector(".add-project");
-    // addProjectModal.style.display = "none";
-
+    //Selection de la balise dialog
     const modal = document.querySelector('#modal');
+
+    //Sélection du bouton qui vas servir à ouvrir les modals
     const openModal = document.querySelector('.open-button');
-    modalAjoutPhoto();
+
+    //Sélection du bouton pour fermer les modals
     const closeModal = document.querySelectorAll('#modal .close-button');
 
+    //Sélection du bouton ajout de photo pour ouvrir la modal .add-project
+    const addProjectModal = document.querySelector(".add-project");
+
+    //Lors du click sur bouton .open-button la fenêtre dialog s'ouvre et appelle des fonctions nécéssaire aux beoisn de la modal gallery
     openModal.addEventListener("click", () => {
         modal.showModal();
+        deleteWorks();
     })
 
+    //Pour chaques boutons de fermeture lors du click la dialog se ferme 
     for (let i = 0; i < closeModal.length; i++) {
         closeModal[i].addEventListener("click", () => {
             modal.close();
+            //La modal gallery apparaît et la modal d'ajout de project disparaît
+            galleryModal.style.display = "flex";
         })
     }
 
+    //Lors d'un click en dehors des modals elles se ferment 
     window.onclick = function (e) {
         if (e.target == modal) {
             modal.close();
-        } else {
-
+            //Remise à la gallery en premier lors de l'ouverture de la modal
+            galleryModal.style.display = "flex";
+            addProjectModal.style.display = "none";
         }
     }
     getWorks();
-    deleteWorks();
+    modalAjoutPhoto();
     postWork();
 
 }
 
 function modalAjoutPhoto() {
-
+    //Sélection du boutton servant à faire apparaître la fenêtre de diagol
     const btnAddWorks = document.querySelector('#ajoutphoto');
-
+    //Sélection de la balise dialog et de la modal à cacher
     const galleryModal = document.querySelector(".gallery-modal");
     const dialogModal = document.querySelector("dialog");
+
+    //Cacher la modal galerie
     galleryModal.style.display = "none";
+    //Création de la modal d'ajout de projet
     const addProjectModal = document.createElement("div");
     addProjectModal.setAttribute("class", "modal-wrapper");
     addProjectModal.classList.add("add-project");
@@ -127,19 +158,38 @@ function modalAjoutPhoto() {
                 <button type="submit" id="valider">Valider</button>
             </form>`
 
+    //Ajout de la modal d'ajour de projet a la balise dialog
     dialogModal.appendChild(addProjectModal);
 
+    // initialisation de la valeur de display pour afficher en premier la modal galerie et faire disparaître la modal d'ajout
+    galleryModal.style.display = "flex";
+    addProjectModal.style.display = "none";
 
+    // Si le bouton de retout est clicker alors la modal gallery apparaît et la modal d'ajout disparraît et la function deleteWorks est appeler
     const returnModal = document.querySelector('.return-button');
     returnModal.addEventListener("click", () => {
         addProjectModal.style.display = "none";
         galleryModal.style.display = "flex";
+        deleteWorks();
     })
 
+    // Si le bouton #ajoutphoto est clicker alors la modal gallery disparaît et la modal d'ajout apparaît
     btnAddWorks.addEventListener("click", () => {
         galleryModal.style.display = "none";
         addProjectModal.style.display = "flex";
     });
+
+    //Selection de la balise dialog
+    const modal = document.querySelector('#modal');
+
+    //Sélection du bouton submit du formulaire d'ajout de photo
+    const submitBtn = document.querySelector('#valider');
+
+    //Lors du click sur bouton sublit la fenêtre dialog se ferme
+    submitBtn.addEventListener("click", () => {
+        modal.close();
+    });
+
 
     //Ajout du code pour faire apparaître la photo mise dans le input file
     const input = document.querySelector(".add-photo input");
@@ -148,41 +198,51 @@ function modalAjoutPhoto() {
     const img = document.querySelector(".add-photo i");
     const text = document.querySelector(".add-photo p");
 
-    input.addEventListener("change", () => {
 
+    input.addEventListener("change", () => {
+        //Lors du click mise en transparence des balise i, label et text
         label.style.opacity = 0;
         img.style.opacity = 0;
         text.style.opacity = 0;
 
         let imgWork = input.files;
         for (let i = 0; i < imgWork.length; i++) {
+            //création d'une balise img avec pour class .imageWork
             let image = document.createElement("img");
+            //définition de ça largeur et hauteur
             image.height = 193;
             image.width = 129;
+            //création d'un aperçu pour l'affichage de l'image grâce l'input 
             image.src = window.URL.createObjectURL(imgWork[i]);
             image.classList.add("imageWork");
+            //ajout de la balise image .imageWork dans le preview
             preview.appendChild(image);
         }
+
+        //lors du click sur le bouton valider les balise label,img et texte pour l'input de l'image sont a nouveau visible
+        submitBtn.addEventListener("click", () => {
+            label.style.opacity = 1;
+            img.style.opacity = 1;
+            text.style.opacity = 1;
+            //suppression de la balise image ou était affiché l'image mise dans l'input file
+            let image = document.querySelector(".imageWork");
+            image.remove();
+            //remise de la vue sur la page de la galerie en faisant disparaître la modal d'ajout de projet
+            galleryModal.style.display = "flex";
+            addProjectModal.style.display = "none";
+        })
+
     })
 
-    // //récupération de toutes les données du formulaire
+    //récupération du formulaire
     const formulairePostWork = document.getElementById("add-form");
-    // formulairePostWork.addEventListener("submit", async function (event) {
-    //     event.preventDefault();
-    //     const work = {
-    //         img: event.target.querySelector('.add-photo input').value,
-    //         title: event.target.querySelector("[name=add-title]").value,
-    //         categoryId: event.target.querySelector("#add-categorie").value
-    //     };
-    //     console.log(work);
-    // })
 
-
-    const submitBtn = document.querySelector("#valider");
+    //récupération de toutes les input du formulaire
     const selectInputs = document.querySelector('#add-form select');
     const titleInputs = document.querySelector('.input-div input');
     const imageInputs = document.querySelector('.add-photo input');
 
+    //vérification que tout les inputs sont remplie si oui alors le bouton valider passe au vert sinon il reste gris
     formulairePostWork.addEventListener("change", () => {
         if (selectInputs.value != "" && titleInputs.value != "" && imageInputs.value != "") {
             submitBtn.style.backgroundColor = "#1d6154";
@@ -195,9 +255,10 @@ function modalAjoutPhoto() {
 
 
 async function getWorks() {
+    //récupération des projets dans le localStorage
     let projets = window.localStorage.getItem('projets')
 
-    if (projets == null) {
+    if (projets === null) {
         // Récupération des projets depuis l'API
         const reponse = await fetch("http://localhost:5678/api/works/");
         projets = await reponse.json();
@@ -209,6 +270,7 @@ async function getWorks() {
         projets = JSON.parse(projets);
     }
 
+    //génération des projets dans la galerie de la modal de suppression
     for (let i = 0; i < projets.length; i++) {
         const work = projets[i];
         const modalWorks = document.querySelector("#modal-gallery").innerHTML += `
@@ -220,19 +282,21 @@ async function getWorks() {
     }
 }
 
-async function deleteWorks() {
+function deleteWorks() {
+    //récupération du token
     let token = window.localStorage.getItem('token');
     token = JSON.parse(token);
     token = token.token;
 
-    let projets = window.localStorage.getItem('projets');
-
+    //récupération dans une constante des boutons de suppression des projets
     const worksElements = document.querySelectorAll("#modal-gallery .projet-selection button");
 
+    //Boucle pour appliquer sur chaques boutons la requête de suppression du projet en question lors du clic
     for (let i = 0; i < worksElements.length; i++) {
         worksElements[i].addEventListener("click", async function (event) {
             const id = event.target.id;
 
+            //requête de suppression à l'API
             fetch("http://localhost:5678/api/works/" + id, {
                 method: "DELETE",
                 headers: {
@@ -241,8 +305,13 @@ async function deleteWorks() {
                 }
             });
 
+            //suppression du projet en question dans la modal #modal-gallery
             const selectedWorksModal = document.querySelector(`#modal-gallery [id='${id}']`).style.display = "none";
+
+            //suppression du projet en question dans la div .gallery
             const selectedWorksGallery = document.querySelector(`.gallery [id='${id}']`).style.display = "none";
+
+            //suppression du projet en question dans le localStorage
             let deleteWork = window.localStorage.removeItem("projets", `[id='${id}]`);
         });
     }
@@ -250,48 +319,79 @@ async function deleteWorks() {
 }
 
 async function postWork() {
-
+    //récupération du token
     let token = window.localStorage.getItem('token');
     token = JSON.parse(token);
     token = token.token;
 
-    // let projets = window.localStorage.getItem('projets');
-    //récupération de toutes les données du formulaire
+    //récupération du formulaire
     let form = document.getElementById("add-form")
+
+    //lors du submit du formulaire
     form.addEventListener("submit", async (e) => {
+
+        // désactivation du comportement par défaut
         e.preventDefault();
 
-        const formData = new FormData(e.currentTarget);
+        //récupération des différents champs du formulaire
+        const imageInput = document.getElementById("image");
+        const titleInput = document.getElementById("title");
+        const categorySelect = document.getElementById("category");
 
-        formData.append(
-            "category", `${e.target.querySelector("#category").value}`
-        )
+        //sélection des values et de l'image pour chaque input
+        const image = imageInput.files[0];
+        const title = titleInput.value;
+        const category = categorySelect.value;
 
-        const formObject = Object.fromEntries(formData);
+        // Créez un objet FormData pour envoyer les données
+        const formData = new FormData();
 
-        console.log(formObject);
+        //ajout des différentes données à formData
+        formData.append("image", image);
+        formData.append("title", title);
+        formData.append("category", category);
 
-        e.currentTarget.reset();
-
-        const headContent = {
-            "Authorization": `BearerAuth ${token}`
-        };
-
+        //requête à l'API pour ajouter les données du formulaire pour l'ajouté aux projets
         const response = await fetch("http://localhost:5678/api/works", {
             method: "POST",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-            },
-            body: formObject
+            headers: { "Authorization": `Bearer ${token}` },
+            body: formData
         });
+
+        //récupération des projets dans le localStorage
+        const projetsExistantsString = localStorage.getItem('projets');
+
+        // mise dans un tableau des projets du localStorage en enlevant les stringify
+        let projetsExistants = [];
+
+        if (projetsExistantsString) {
+            projetsExistants = JSON.parse(projetsExistantsString);
+        }
         const data = await response.json();
 
+        //ajout des données du nouveau projet venant de l'API aux données de projets déjà existants
+        projetsExistants.push(data);
 
-        console.log(data)
+        //Ajout au local storage des projets avec le nouveau projets dans l'item projets du localStorage
+        localStorage.setItem("projets", JSON.stringify(projetsExistants));
 
-        // const value = await response.json();
-        // const projectItem = JSON.stringify(value);
+        //Ajout à la galerie du modal du nouveau projet en l'affichant
+        const modalWorks = document.querySelector("#modal-gallery").innerHTML += `
+            <div class="projet-selection" id="${data.id}">
+                <img class="work-img" src=${data.imageUrl} alt="${data.title}">
+                <button class="delete-work" id="${data.id}"><i id="${data.id}" class="fa-solid fa-trash-can"></i></button>
+            </div>
+        `
+        //Ajout à la galerie de la page de modification du nouveau projet en l'affichant
+        const sectionGallery = document.querySelector(".gallery").innerHTML += `
+                    <figure id="${data.id}">
+                        <img src=${data.imageUrl} alt="${data.title}">
+                        <figcaption>${data.title}</figcaption>
+                    </figure>`
 
-        // window.localStorage.setItem("projets", projectItem);
+        //suppression des valeurs dans les champs titre et categorie
+        titleInput.value = "";
+        categorySelect.value = "";
     });
+
 }
